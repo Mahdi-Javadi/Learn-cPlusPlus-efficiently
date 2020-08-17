2d07a264a2d715d47134a652fce6194aff69ea40
MahdiJavadi <javadimahdi1991@gmail.com>
2020-08-17 23:29:15 +0200

    Day5-modified cpp file

--- Day5/Arrays.cpp
+++ Day5/Arrays.cpp
@@ -2,7 +2,7 @@
 using std::cout;
 using std::endl;
 int main(){
-    cout<< "I want to print out the elements of my array:\n ";
+    cout<< "I want to print out the elements of my array:\n";
     int my_array [] {10,20,30,40};
     cout<< "Element1_index0:  "<<my_array[0]<<endl;
     cout<< "Element2_index1:  "<<my_array[1]<<endl;
