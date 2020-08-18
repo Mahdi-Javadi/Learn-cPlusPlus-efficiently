3f1eb1d857a199deda995fa7a355e34794a98fb9
MahdiJavadi <javadimahdi1991@gmail.com>
2020-08-18 22:33:14 +0200

    Day2- printing new lines and comment added

--- Day2/PrintNumber.cpp
+++ Day2/PrintNumber.cpp
@@ -7,7 +7,9 @@ int main(){
     cout << "Please enter an integer number:";
     int number;
     cin >> number;
-    cout << "You have entered: "<< number<<endl;
+    cout << "You have entered: "<< number<<endl; 
+    cout<< " I want to add a new line.\n";// We can use \n to add a new line in the output
+    cout << "*****\n&&&&&\n$$$$$\n#####\n@@@@@\n";
     return 0;
 }
 
